7faea08ea420ae5b9ca602c45ba8b73540fe7518
清川　裕太郎 <yuu7353@gmail.com>
2020-12-13 19:18:05 +0900

    setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,25 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `ESSENTIALS`,
+    description: ` おいしい食材と食事を探求するサイト`,
+    lang: `ja`,
+    siteUrl: `https://********.netlify.app`,
+    locale: `ja_JP`,
+    fbappid: `XXXXXXXXXXXXXXXXXXXXX`,
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
